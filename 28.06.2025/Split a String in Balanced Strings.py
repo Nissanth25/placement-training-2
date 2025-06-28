7776d9f2dcfc283c6c98daea5e510f4484de0c3e
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2025-06-28 14:18:04 +0530

    Create Split a String in Balanced Strings.py

---
+++ 28.06.2025/Split a String in Balanced Strings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def balancedStringSplit(self, s):
+        x=0
+        c=0
+        for i in range(len(s)):
+            if s[i]=='L':
+                x+=1
+            else:
+                x-=1
+            if x==0:
+                c+=1
+        return c
